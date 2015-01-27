74d28ff4bbdd6863dec2fec8aad1c0be3ab7a943
tkartikreddy <kartiktipparthi@gmail.com>
2015-01-27 12:24:46 -0500

    In response to  #5.

--- gen/com/teammatharu/cookbook/R.java
+++ gen/com/teammatharu/cookbook/R.java
@@ -22,21 +22,32 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int buttongradient=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int btn1=0x7f080002;
+        public static final int btn2=0x7f080003;
+        public static final int btn3=0x7f080004;
+        public static final int btn4=0x7f080005;
+        public static final int imageView1=0x7f080001;
+        public static final int scrollView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main_landing_page=0x7f030000;
     }
     public static final class menu {
-        public static final int main_landing_page=0x7f070000;
+        public static final int content=0x7f070000;
+        public static final int main_landing_page=0x7f070001;
+        public static final int selection_grid=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_content=0x7f050004;
+        public static final int title_activity_selection_grid=0x7f050003;
     }
     public static final class style {
         /** 
