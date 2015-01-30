c4c2f0bebc8e3d8346ff9c6513cb0ec878786691
tkartikreddy <kartiktipparthi@gmail.com>
2015-01-30 18:48:16 -0500

    adding image in sub menu

--- gen/com/teammatharu/cookbook/R.java
+++ gen/com/teammatharu/cookbook/R.java
@@ -26,13 +26,13 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090015;
+        public static final int action_settings=0x7f090016;
         public static final int btn1=0x7f090007;
-        public static final int btn10=0x7f090010;
-        public static final int btn11=0x7f090011;
-        public static final int btn12=0x7f090012;
-        public static final int btn13=0x7f090013;
-        public static final int btn14=0x7f090014;
+        public static final int btn10=0x7f090011;
+        public static final int btn11=0x7f090012;
+        public static final int btn12=0x7f090013;
+        public static final int btn13=0x7f090014;
+        public static final int btn14=0x7f090015;
         public static final int btn2=0x7f090008;
         public static final int btn3=0x7f090009;
         public static final int btn4=0x7f09000a;
@@ -40,13 +40,14 @@ public final class R {
         public static final int btn6=0x7f09000c;
         public static final int btn7=0x7f09000d;
         public static final int btn8=0x7f09000e;
-        public static final int btn9=0x7f09000f;
+        public static final int btn9=0x7f090010;
         public static final int btnCourses=0x7f090002;
         public static final int btnMain_Ingredients=0x7f090004;
         public static final int btnMeals=0x7f090003;
         public static final int btnOcc_Cooking=0x7f090005;
         public static final int imageView1=0x7f090001;
         public static final int imageViewSubMeal=0x7f090006;
+        public static final int imageViewSubMenu=0x7f09000f;
         public static final int scrollView1=0x7f090000;
     }
     public static final class layout {
