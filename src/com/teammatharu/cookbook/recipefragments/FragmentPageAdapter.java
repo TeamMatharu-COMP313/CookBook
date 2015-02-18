1f9ec09e062765aae8caa24624a64847219c7898
Hiren Patel <patelh18@gmail.com>
2015-02-18 16:17:20 -0500

    Added +package:com.teammatharu.cookbook.recipefragments + FragmentPageAdapter.java +RecipeDirection.java +fragment_recipe_direction.xml

---
+++ src/com/teammatharu/cookbook/recipefragments/FragmentPageAdapter.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.teammatharu.cookbook.recipefragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * @author Hiren
+ *
+ */
+public class FragmentPageAdapter extends FragmentPagerAdapter {
+
+	/**
+	 * @param fm
+	 */
+	public FragmentPageAdapter(FragmentManager fm) {
+		super(fm);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.FragmentPagerAdapter#getItem(int)
+	 */
+	@Override
+	public Fragment getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see android.support.v4.view.PagerAdapter#getCount()
+	 */
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/com/teammatharu/cookbook/recipefragments/RecipeDirection.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.teammatharu.cookbook.recipefragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * @author Hiren
+ *
+ */
+public class RecipeDirection extends Fragment {
+
+	/**
+	 * 
+	 */
+	public RecipeDirection() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+
+}
