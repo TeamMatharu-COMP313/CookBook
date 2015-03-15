bbce01bf513a3901b51f85d07c048e11c6012d10
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-03-15 00:25:16 -0400

    Updating Contact US page

--- src/com/teammatharu/cookbook/ContactUs.java
+++ src/com/teammatharu/cookbook/ContactUs.java
@@ -18,25 +18,32 @@ public class ContactUs extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contact_us);
-		
-		emailUs=(Button) findViewById(R.id.emailus);
-		
-		
+
+		emailUs = (Button) findViewById(R.id.emailus);
+
 		emailUs.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent intent = new Intent(Intent.ACTION_SENDTO); // it's not ACTION_SEND
+				Intent intent = new Intent(Intent.ACTION_SENDTO); // it's not
+																	// ACTION_SEND
 				intent.setType("text/plain");
 				intent.putExtra(Intent.EXTRA_SUBJECT, "");
 				intent.putExtra(Intent.EXTRA_TEXT, "Message:");
-				intent.setData(Uri.parse("mailto:patelh18@gmail.com;urvashi.ucoe@gmail.com;jagdeepmatharu88@gmail.com;kartiktipparthi@gmail.com;parin2392@gmail.com;abhi.mekwan@gmail.com;")); // or just "mailto:" for blank
+				intent.setData(Uri
+						.parse("mailto:patelh18@gmail.com;urvashi.ucoe@gmail.com;jagdeepmatharu88@gmail.com;kartiktipparthi@gmail.com;parin2392@gmail.com;abhi.mekwan@gmail.com;")); // or
+																																														// just
+																																														// "mailto:"
+																																														// for
+																																														// blank
 				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 				try {
-				    startActivity(Intent.createChooser(intent, "Send mail..."));
+					startActivity(Intent.createChooser(intent, "Send mail..."));
 				} catch (android.content.ActivityNotFoundException ex) {
-				    Toast.makeText(ContactUs.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+					Toast.makeText(ContactUs.this,
+							"There are no email clients installed.",
+							Toast.LENGTH_SHORT).show();
 				}
 			}
 		});
@@ -45,7 +52,7 @@ public class ContactUs extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(R.menu.content, menu);
+		// getMenuInflater().inflate(R.menu.content, menu);
 		return true;
 	}
 
